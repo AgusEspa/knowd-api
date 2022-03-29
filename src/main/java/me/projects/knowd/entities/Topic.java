02d2893b3ddbe1c6df5564bd497809bbfe2c3162
Agustin Espana <agustinespania@gmail.com>
2022-03-29 13:36:30 -0300

    added constraints for patch methods

--- src/main/java/me/projects/knowd/entities/Topic.java
+++ src/main/java/me/projects/knowd/entities/Topic.java
@@ -2,10 +2,7 @@ package me.projects.knowd.entities;
 
 
 import javax.persistence.*;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 @Entity
