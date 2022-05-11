608a9a15a81d39a0d6703e75e5968f5dd191de25
Agustin Espana <agustinespania@gmail.com>
2022-05-11 19:33:06 -0300

    cleaned code

--- src/main/java/me/projects/knowd/Application.java
+++ src/main/java/me/projects/knowd/Application.java
@@ -1,16 +1,7 @@
 package me.projects.knowd;
 
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
@@ -18,5 +9,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
