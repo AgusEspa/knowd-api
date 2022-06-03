e45ef8c46c6a2a52c05bba67b6dd1106f3591593
Agustin Espana <agustinespania@gmail.com>
2022-06-03 13:11:23 -0300

    Update Token.java

--- src/main/java/me/projects/knowd/entities/Token.java
+++ src/main/java/me/projects/knowd/entities/Token.java
@@ -1,15 +1,47 @@
 package me.projects.knowd.entities;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 
-@Entity
-class Token {
-    String tokenString;
+import java.util.Objects;
 
+@Entity
+@Table(name = "revoqued_tokens")
+class Token {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "token_id")
+    private Long id;
+
+    @NotBlank
+    String tokenString;
 
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
     public Token(String tokenString) {
         this.tokenString = tokenString;
+    }
+
+    public String getTokenString() {
+        return tokenString;
+    }
+
+    public void setTokenString(String tokenString) {
+        this.tokenString = tokenString;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Token token = (Token) o;
+        return Objects.equals(tokenString, token.tokenString);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tokenString);
     }
 }
